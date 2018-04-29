58b7d04fe4a88cb6eed399c42dfe45118a2e09f5
alb alb <albert_nik@hotmail.com>
2018-04-29 22:04:25 +0100

    add inches-to cm

---
+++ Nba.Tests/TestDistance.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nba.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nba.Tests
+{
+   [TestClass]
+   public class TestDistance
+   {
+      [TestMethod]
+      public void ParseDistance()
+      {
+         Distance d = Distance.FromFeet("6 ft 2 in");
+         Assert.AreEqual(d.Feets, 6);
+         Assert.AreEqual(d.Inches, 2);
+
+
+         Assert.ThrowsException<ArgumentException>(() => { Distance.FromFeet("bad data 123"); });
+      }
+
+      [TestMethod]
+      public void DistanceConversion()
+      {
+         Distance d = new Distance();
+         Assert.AreEqual(d.Feets, 0);
+         Assert.AreEqual(d.Inches, 0);
+
+         d = new Distance(3, 5);
+         Assert.AreEqual(d.Feets, 3);
+         Assert.AreEqual(d.Inches, 5);
+
+         Assert.AreEqual(d.ToCm(), 104.14, 0.001);
+      }
+   }
+}
---
+++ Nba/Entities/distance.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nba.Entities
+{
+   public class Distance
+   {
+      public int Feets { get; private set; }
+      public int Inches { get; private set; }
+
+      public Distance()
+         : this(0, 0)
+      { }
+
+      public Distance(int feet, int inches)
+      {
+         Feets = feet;
+         Inches = inches;
+      }
+
+
+      public static Distance FromFeet(string s)
+      {
+         var parts = s.Split(' ');
+
+         if(parts.Length == 4)
+         {
+            int feet = 0;
+            int inches = 0;
+            if(int.TryParse(parts[0], out feet) && int.TryParse(parts[2], out inches))
+               return new Distance(feet, inches);
+         }
+
+
+         throw new ArgumentException("format exception", s);
+
+      }
+
+      public double ToCm()
+      {
+         return (12 * Feets + Inches) * 2.54;
+      }
+   }
+}
